3fc928e0f34dc0a591de626ea6dc54d9a9fbc17a
KashbullinRail <kashbullinrail@gmail.com>
2022-09-14 08:38:09 +0300

    организация подписчиков простым способом - применение map

--- app/src/main/java/com/example/rxjava_example/MainActivity.kt
+++ app/src/main/java/com/example/rxjava_example/MainActivity.kt
@@ -12,22 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val someObservable = Observable.just(1,2,3,4,5,6,7,8,9)
+        val someObservable = Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
 
         someObservable.filter {
             it % 3 == 0
-        }.subscribe(
-            {
-            Log.d("RxJava3", "onNext $it")
-            },
-            {
-
-            },
-            {
-
-            }
-        )
+        }.map {
+            it.toDouble()
+        }
+            .subscribe(
+                {
+                    Log.d("RxJava3", "onNext $it")
+                },
+                {},
+                {}
+            )
 
     }
 }
