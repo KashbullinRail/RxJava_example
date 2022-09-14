e38c86c4c34014c2e3031f87ca029064f84e7490
KashbullinRail <kashbullinrail@gmail.com>
2022-09-14 10:18:20 +0300

    организация подписчиков простым способом - добавление Flowable

--- app/src/main/java/com/example/rxjava_example/MainActivity.kt
+++ app/src/main/java/com/example/rxjava_example/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.core.Scheduler
@@ -21,15 +22,25 @@ class MainActivity : AppCompatActivity() {
 
         someObservable
             .subscribeOn(Schedulers.newThread())
+            .doOnNext {
+                Log.d("RxJava3", "${Thread.currentThread().name}")
+            }
             .observeOn(Schedulers.newThread())
             .doOnNext {
                 Log.d("RxJava3", "${Thread.currentThread().name}")
             }
-
             .subscribe(
                 {
                     Log.d("RxJava3", "onNext $it")
                 },
+                {},
+                {}
+            )
+        Flowable.just(1, 1, 3, 4, 1, 1, 7, 8, 9)
+            .onBackpressureBuffer(4)
+            .subscribe({
+                Log.d("RxJava3", "doOnNextFlow $it")
+            },
                 {},
                 {}
             )
